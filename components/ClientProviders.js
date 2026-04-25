52bce728d06e33be7214e828fb182f581b420584
Neofort <dan.calutu@gmail.com>
2026-04-25 23:37:31 +0300

    v55: fix build - ClientProviders wrapper pentru WAFloat si MobileBar

---
+++ components/ClientProviders.js
@@ -0,0 +1,12 @@
+'use client'
+import WAFloat from './WAFloat'
+import MobileBar from './MobileBar'
+
+export default function ClientProviders() {
+  return (
+    <>
+      <WAFloat />
+      <MobileBar />
+    </>
+  )
+}
